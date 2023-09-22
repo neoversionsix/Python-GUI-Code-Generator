22a9604b14aee65b0f8220a8221ee8f0a081cd4f
Jason Whittle <jason.j.whittle@gmail.com>
2023-09-22 12:41:37 +1000

    up

--- Code Generator.py
+++ Code Generator.py
@@ -5,19 +5,18 @@ import pyperclip
 
 class App:
     def __init__(self, root):
+        # Button to load Excel file
+        self.load_button = tk.Button(root, text="Load XLSX File", command=self.load_xlsx)
+        self.load_button.pack(pady=10)
+        
+        # Label to display loaded file path
+        self.filepath_label = tk.Label(root, text="")
+        self.filepath_label.pack(pady=5)
+        
         # Label and Textbox for input template
         tk.Label(root, text="PASTE TEMPLATE CODE HERE").pack(pady=10)
         self.template_text = tk.Text(root, height=10, width=50)
         self.template_text.pack(pady=10)
-        
-        # Label and Textbox for output
-        tk.Label(root, text="GENERATED CODE").pack(pady=10)
-        self.output_text = tk.Text(root, height=10, width=50)
-        self.output_text.pack(pady=10)
-        
-        # Button to load Excel file
-        self.load_button = tk.Button(root, text="Load XLSX File", command=self.load_xlsx)
-        self.load_button.pack(pady=10)
 
         # Button to run the process
         self.run_button = tk.Button(root, text="RUN", command=self.generate_code)
@@ -31,6 +30,7 @@ class App:
         if not filepath:
             return
         self.df = pd.read_excel(filepath)
+        self.filepath_label.config(text=filepath)  # Display the loaded file path
     
     def generate_code(self):
         if self.df is None:
@@ -47,8 +47,6 @@ class App:
             generated_codes.append(code)
         
         output = ('\n' * 3).join(generated_codes)
-        self.output_text.delete("1.0", "end")
-        self.output_text.insert("1.0", output)
         
         pyperclip.copy(output)
         messagebox.showinfo("Info", "Code generated and saved to clipboard. Paste with ctrl-v.")
